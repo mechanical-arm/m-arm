6cd680e54fb6b8c314160db685dc7a99546525e4
Tommaso Paoli <paoli7612@gmail.com>
2018-05-08 20:21:05 +0200

    socket connection completed

--- src/socket/client.py
+++ src/socket/client.py
@@ -1,5 +1,20 @@
 import socket
+from time import sleep
 
 class Client:
     def __init__(self):
-        pass
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_adress = ("localhost", 7612)
+        self.running = True
+
+    def run(self):
+        self.sock.connect(self.server_adress)
+        while self.running:
+            data = self.sock.recv(1024)
+            message = data.decode()
+            print(message)
+            sleep(0.4)
+
+c = Client()
+
+c.run()
--- src/socket/server.py
+++ src/socket/server.py
@@ -1,5 +1,25 @@
 import socket
-
+from time import sleep
 class Server:
     def __init__(self):
-        pass
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.address = ("localhost", 7612)
+        self.sock.bind(self.address)
+        self.sock.listen(1)
+        self.running = True
+
+    def run(self):
+        self.client, self.client_address = self.sock.accept()
+        print("connect to %s" % str(self.client_address))
+        while self.running:
+            message = "ciao"
+            data = message.encode()
+            try: self.client.send(data)
+            except: self.running = False
+            sleep(0.4)
+        print("connection lost")
+
+
+
+s = Server()
+s.run()
