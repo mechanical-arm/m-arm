83a94d6a0973c5e565be55117d75f2c300541a36
Tommaso Paoli <paoli7612@gmail.com>
2018-05-08 19:42:20 +0200

    get table from data

--- src/data.py
+++ src/data.py
@@ -1,4 +1,21 @@
+import os
+import xml.etree.ElementTree as et
+
+from table import Table
 
 class Data:
     def __init__(self):
-        pass
+        name_xml = "tables.xml"
+        folder_xml = "data"
+        path = os.path.dirname(__file__)
+        self.path_xml = os.path.join(path, folder_xml, name_xml)
+        print(self.get_table("4"))
+
+    def get_table(self, id):
+        tree = et.parse(self.path_xml)
+        root = tree.getroot()
+        for element in root:
+            if id == element.attrib["id"]:
+                return Table(id, element.text)
+
+d = Data()
--- src/table.py
+++ src/table.py
@@ -1,4 +1,25 @@
 
 class Table:
-    def __init__(self):
-        pass
+    def __init__(self, id, text):
+        self.matrix = self.parse(text)
+
+
+
+    def parse(self, text):
+        # remove tab
+        try: text = text.translate(None, '\t')
+        except: text = text.translate('\t')
+        # split line
+        text = text.split("\n")
+        # split cell
+        for p,line in enumerate(text):
+            text[p] = line.split()
+        return text[1:-1]
+
+    def __str__(self):
+        text = str()
+        for line in self.matrix:
+            for cell in line:
+                text += cell + "\t"
+            text += "\n"
+        return text
